f3f3f7f3b20faa85f09eca1d19a7f5074ac5c284
Sharif Akram <rifkit@gmail.com>
2024-01-06 11:35:16 -0500

    add code from 05 - Scrapy

---
+++ 05/wikiSpider/wikiSpider/article.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class ArticleSpider(scrapy.Spider):
+    name = "article"
+
+    def start_requests(self):
+        urls = [
+            "http://en.wikipedia.org/wiki/Python_%28programming_language%29",
+            "https://en.wikipedia.org/wiki/Functional_programming",
+            "https://en.wikipedia.org/wiki/Monty_Python",
+        ]
+        return [scrapy.Request(url=url, callback=self.parse) for url in urls]
+
+    def parse(self, response):
+        url = response.url
+        title = response.css(
+            "h1::text, #firstHeading::text, title::text"
+        ).extract_first()
+        if title:
+            print("URL is : {}".format(url))
+            print("Title is: {}".format(title))
+        else:
+            self.log(f"Title not found for URL: {url}")
